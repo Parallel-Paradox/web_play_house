55df661e3e9c89594d17467c124969367db0fcd2
Parallel-Paradox <q1203981754@hotmail.com>
2022-08-07 10:48:57 +0800

    [Component] From uiverse.io by @Voxybuns

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Button_1/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Button_1 from './components/button/Button_1.vue';
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        Button_1,
+    },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
